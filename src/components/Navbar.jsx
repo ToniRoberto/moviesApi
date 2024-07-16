e927b9a21387e1006c5fdaee612809d71f9eb06a
Toni Roberto <tonirsc78@gmail.com>
2024-07-16 17:35:43 -0300

    APIFilmes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link, useNavigate } from "react-router-dom"
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+import "./Nav.css"
+import { useState } from "react"
+
+const Navbar = () => {
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    if(!search) return
+      navigate(`/search?q=${search}`)
+      setSearch("")
+  }
+  return (
+    <nav id="navbar">
+        <h2>
+          <Link to="/"><BiCameraMovie></BiCameraMovie> Movies API</Link>
+        </h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)} value={search}/>
+            <button type="submit"><BiSearchAlt2></BiSearchAlt2></button>
+        </form>
+      </nav>
+  )
+}
+
+export default Navbar
